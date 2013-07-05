5f6822ef8271f04770671a42a6eafc4e247e81c1
Masato Nagai <masato.nagai@gree.net>
2013-07-05 18:34:55 +0900

    do not use sprite batch

--- character/Classes/Action1.cpp
+++ character/Classes/Action1.cpp
@@ -23,56 +23,24 @@ bool Action1::init()
 {
     printf("init()");
     
-    CCSpriteBatchNode* spritebatch = CCSpriteBatchNode::create("darknight.png");
     CCSpriteFrameCache* spriteCache = CCSpriteFrameCache::sharedSpriteFrameCache();
     spriteCache->addSpriteFramesWithFile("darknight.plist");
     
-    char str[100] = {0};
-    CCSprite *sprite;
-    for (int i = 1; i <= 7; i++) {
-        sprintf(str, "%d.png", i);
-        printf("add frame %s", str);
-        sprite = CCSprite::createWithSpriteFrameName(str);
-        spritebatch->addChild(sprite);
-    }
-    this->addChild(spritebatch);
-    
     CCArray* animFrames = CCArray::createWithCapacity(7);
+    char str[100] = {0};
     for (int i = 1; i <= 7; i++) {
         sprintf(str, "%d.png", i);
         CCSpriteFrame* frame = spriteCache->spriteFrameByName(str);
         animFrames->addObject(frame);
     }
     
+    CCSprite* sprite = CCSprite::createWithSpriteFrameName(str);
     CCSize winSize = CCDirector::sharedDirector()->getWinSize();
-    sprite->setPosition(ccp(sprite->getContentSize().width/2, winSize.height/2));
+    sprite->setPosition(ccp(100, 100));
+    this->addChild(sprite);
     
     CCAnimation* animation = CCAnimation::createWithSpriteFrames(animFrames, 0.1f);
-    sprite->runAction( CCRepeatForever::create( CCAnimate::create(animation) ) );
-    
+    sprite->runAction(CCRepeatForever::create(CCAnimate::create(animation)));
     
     return true;
 }
-/*
- CCAnimationCache *cache = CCAnimationCache::sharedAnimationCache(); // "caches" are always singletons in cocos2d
- 2cache->addAnimationsWithFile("animations/animations-2.plist");
- 3CCAnimation animation = cache->animationByName("dance_1");  // I apologize for this method name, it should be getAnimationByName(..) in future versions
- 4CCAnimate animate = CCAnimate::create(animation);  // Don't confused between CCAnimation and CCAnimate :)
- 5sprite->runAction(animate);
- 
- 1CCAnimation *animation = CCAnimation::create();
- 2
- 3// load image file from local file system to CCSpriteFrame, then add into CCAnimation
- 4for (int i = 1; i < 15; i++)
- 5{
- 6    char szImageFileName[128] = {0};
- 7    sprintf(szImageFileName, "Images/grossini_dance_%02d.png", i);
- 8    animation->addSpriteFrameWithFileName(szImageFileName);
- 9}
- 10
- 11animation->setDelayPerUnit(2.8f / 14.0f); // This animation contains 14 frames, will continuous 2.8 seconds.
- 12animation->setRestoreOriginalFrame(true); // Return to the 1st frame after the 14th frame is played.
- 13
- 14CCAnimate *action = CCAnimate::create(animation);
- 15sprite->runAction(action);  // run action on sprite object
- */
