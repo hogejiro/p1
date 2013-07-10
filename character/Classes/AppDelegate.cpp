f3ab25f5641c013de2c0eda6b2fb7ec174d76fa4
Masato Nagai <masato.nagai@gree.net>
2013-07-10 18:18:22 +0900

    finished the first step of character mod

--- character/Classes/AppDelegate.cpp
+++ character/Classes/AppDelegate.cpp
@@ -10,7 +10,7 @@
 
 #include "cocos2d.h"
 #include "SimpleAudioEngine.h"
-#include "Action2.h"
+#include "BattleScene.h"
 
 USING_NS_CC;
 using namespace CocosDenshion;
@@ -37,7 +37,7 @@ bool AppDelegate::applicationDidFinishLaunching()
     pDirector->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    CCScene *pScene = Action2::scene();
+    CCScene *pScene = BattleScene::scene();
 
     // run
     pDirector->runWithScene(pScene);
--- character/Classes/BattleScene.h
+++ character/Classes/BattleScene.h
@@ -1,21 +1,23 @@
 //
-//  Action1.h
+//  BattleScene.h
 //  proto1
 //
 //  Created by Masato Nagai on 2013/07/02.
 //
 //
 
-#ifndef proto1_Action1_h
-#define proto1_Action1_h
+#ifndef proto1_BattleScene_h
+#define proto1_BattleScene_h
 
 #include "cocos2d.h"
 
-class Action1 : public cocos2d::CCLayer
+class BattleScene : public cocos2d::CCLayer
 {
 public:
     // Method 'init' in cocos2d-x returns bool, instead of 'id' in cocos2d-iphone (an object pointer)
     virtual bool init();
+    virtual void addOne();
+    virtual void addTwo();
     
     // there's no 'id' in cpp, so we recommend to return the class instance pointer
     static cocos2d::CCScene* scene();
@@ -24,7 +26,8 @@ public:
     void menuCloseCallback(CCObject* pSender);
     
     // preprocessor macro for "static create()" constructor ( node() deprecated )
-    CREATE_FUNC(Action1);
+    CREATE_FUNC(BattleScene);
 };
+
 
 #endif
